855a29ee51a87fa904e79ddffcec1f5dc0dbb528
mohammad haroon khan <32263261+haroon0403@users.noreply.github.com>
2026-05-27 12:53:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello():
 # Route to a custom endpoint
 @app.route('/greet/<name>')
 def greet(name):
-    return f'Hello, {name}! Welcome to Flask on Docker.'
+    return f'Hello, {name}! Welcome to Flask on Docker 2.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
